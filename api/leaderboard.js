7c9c548d07bc8129ac2851c289f4b64f8b3f8a64
smsgithu <seanmsuvie@gmail.com>
2026-01-20 07:21:41 -0500

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   try {
     const result = await sql`
-      SELECT wallet_address, xp, total_questions as questions_asked 
+      SELECT wallet_address, xp, total_questions as questions_asked FROM users WHERE xp > 0 ORDER BY xp DESC LIMIT 50
       FROM users 
       WHERE xp > 0
       ORDER BY xp DESC 
